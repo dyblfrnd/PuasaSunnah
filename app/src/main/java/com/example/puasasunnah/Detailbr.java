bc0b6f8c3da33051ca864e109d328bade478c24e
dyblfrnd <safaraz.afma.affandi@gmail.com>
2019-05-09 09:50:59 +0700

    n

--- app/src/main/java/com/example/puasasunnah/Detailbr.java
+++ app/src/main/java/com/example/puasasunnah/Detailbr.java
@@ -1,13 +1,25 @@
 package com.example.puasasunnah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Detailbr extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailbr);
+        webView = findViewById(R.id.webview);
+
+        Intent detail = getIntent();
+        detail.getIntExtra("posisi", 0);
+        String link = detail.getStringExtra("link");
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(link);
+
     }
 }
